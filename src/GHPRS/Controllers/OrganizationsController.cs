9fe643c99c184617dea94830c8769373c71c12e1
Brian Mwasi <mwasi.brian@gmail.com>
2021-01-29 15:20:26 +0300

    updated controller

--- src/GHPRS/Controllers/OrganizationsController.cs
+++ src/GHPRS/Controllers/OrganizationsController.cs
@@ -4,7 +4,6 @@ using GHPRS.Core.Entities;
 using GHPRS.Core.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -17,18 +16,16 @@ namespace GHPRS.Controllers
     {
         private readonly ILogger<OrganizationsController> _logger;
         private readonly IOrganizationRepository _organizationRepository;
-        private readonly UserManager<User> _userManager;
 
         public OrganizationsController(ILogger<OrganizationsController> logger,
-            IOrganizationRepository organizationRepository, UserManager<User> userManager)
+            IOrganizationRepository organizationRepository)
         {
             _logger = logger;
             _organizationRepository = organizationRepository;
-            _userManager = userManager;
         }
 
         [HttpGet("{id}")]
-        public object GetbyId(int id)
+        public object GetById(int id)
         {
             return _organizationRepository.GetById(id);
         }
@@ -40,12 +37,12 @@ namespace GHPRS.Controllers
         }
 
         [HttpPost]
-        public IActionResult newOrganisation([FromBody] Organization organisation)
+        public IActionResult RegisterOrganization([FromBody] Organization organization)
         {
             try
             {
-                _organizationRepository.Insert(organisation);
-                return Ok(organisation);
+                _organizationRepository.Insert(organization);
+                return Ok(organization);
             }
             catch (Exception e)
             {
