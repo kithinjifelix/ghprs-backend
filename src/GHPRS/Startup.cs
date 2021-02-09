2457b3091d01ed0c84f9a8407ed105a1e600b487
Brian Mwasi <mwasi.brian@gmail.com>
2021-02-09 10:07:58 +0300

    Automatic migration

--- src/GHPRS/Startup.cs
+++ src/GHPRS/Startup.cs
@@ -107,8 +107,10 @@ namespace GHPRS
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory, GhprsContext context)
         {
+            context.Database.Migrate();
+
             if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
 
             app.UseCors(MyAllowSpecificOrigins);
